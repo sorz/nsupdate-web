47f8d5034fde9ef72e4ec150b8c50d519323a058
Zack Cerza <zack@redhat.com>
2016-12-20 16:48:23 -0700

    Add setup.py
    Signed-off-by: Zack Cerza <zack@redhat.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requirements = [line.strip() for line in
+                open('requirements.txt', 'r').readlines()]
+
+
+setup_args = dict(
+    name='nsupdate-web',
+    version='0.1',
+    description='Simple DDNS (dynamic DNS) web API service with nsupdate',
+    author="Shell Chen",
+    author_email="me@sorz.org",
+    url="https://github.com/sorz/nsupdate-web",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points=dict(
+        console_scripts=[
+            'ddns-server = nsupdate_web.server:main',
+        ]
+    ),
+)
+
+setup(**setup_args)
